51157e05c382242e644771cd77351b81ceed05d5
Ravi Shanker B <ravishanker414@gmail.com>
2015-04-23 13:56:16 +0530

    Make the library installable with setup.py and prefix it with rbox

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+setup(
+    name='rbox_htmldiff',
+    version='0.0',
+    py_modules=['rbox_htmldiff', ],
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+)
